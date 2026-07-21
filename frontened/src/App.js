c9b9f345feaf9f17619697221c623ddcf4bb4761
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-07-22 00:18:48 +0500

    Add About and Contact Pages

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Shoes from "./components/Shoes/Shoes";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 
 const App = () => {
 
   return (
     <Routes>
+      <Navbar/>
 
       {/* Home Page */}
 
