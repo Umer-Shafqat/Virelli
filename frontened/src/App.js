609200acb8a92b2163e5ef31738250d82c6b6c52
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-07-22 00:32:20 +0500

    Add folders for more functionalities

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,34 +1,20 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Shoes from "./components/Shoes/Shoes";
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
 
-const App = () => {
-
+function App() {
   return (
-    <Routes>
-      <Navbar/>
+    <>
+      {/* Navbar is outside Routes */}
+      <Navbar />
 
-      {/* Home Page */}
-
-      <Route
-        path="/"
-        element={<Home />}
-      />
-
-      {/* All Shoes Page */}
-
-      <Route
-        path="/shoes"
-        element={<Shoes />}
-      />
-
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shoes" element={<Shoes />} />
+      </Routes>
+    </>
   );
-};
+}
 
 export default App;
