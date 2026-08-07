c1e51aeafd63e8677eff853592f54daf14e0da85
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-08-07 21:18:05 +0500

    Edit newarrival

--- frontened/src/pages/NewArrival/NewArrival.jsx
+++ frontened/src/pages/NewArrival/NewArrival.jsx
@@ -1,45 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Shoes from "../../components/Shoes/Shoes";
+import Footer from "../../components/Footer/Footer";
+import "./NewArrival.css";
 
 const NewArrival = () => {
+  const url = "http://localhost:4000";
 
-    const url = "http://localhost:4000";
+  const [shoes, setShoes] = useState([]);
 
-    const [shoes, setShoes] = useState([]);
+  useEffect(() => {
+    fetchNewArrivals();
+  }, []);
 
-    useEffect(() => {
-        fetchNewArrivals();
-    }, []);
+  const fetchNewArrivals = async () => {
+    try {
+      const res = await axios.get(`${url}/api/shoes/new-arrivals`);
 
-    const fetchNewArrivals = async () => {
-        const res = await axios.get(
-            `${url}/api/shoes/new-arrivals`
-        );
+      if (res.data.success) {
+        setShoes(res.data.shoes);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        if (res.data.success) {
-            setShoes(res.data.shoes);
-        }
-    };
+  return (
+    <div className="newarrival-page">
 
-    return (
-        <div className="new-arrivals">
+      <div className="newarrival-content">
+        <h2 className="newarrival-title">New Arrivals</h2>
 
-            <h2>New Arrivals</h2>
-
-            <div className="shoe-grid">
-
-                {shoes.map((shoe) => (
-                    <Shoes
-                        key={shoe._id}
-                        shoe={shoe}
-                    />
-                ))}
-
-            </div>
-
+        <div className="shoe-grid">
+          {shoes.length > 0 ? (
+  shoes.map((shoe) => (
+    <Shoes
+      key={shoe._id}
+      shoe={shoe}
+    />
+  ))
+) : (
+  <h3 className="empty-message">
+    No New Arrival Shoes Available
+  </h3>
+)}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NewArrival;
