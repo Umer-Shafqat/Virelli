fc83966d7c4a86c424a15b90a93b281eaba3561d
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-08-13 18:35:22 +0500

    adminroute

--- backened/routes/adminRoute.js
+++ backened/routes/adminRoute.js
@@ -1,7 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import adminMiddleware from "../middleware/adminMiddleware.js";
-
 import {
   getDashboard,
   getAllUsers,
