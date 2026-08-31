2d213a712fdfc831ac127efd615687e44dba6510
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-08-31 17:14:30 +0500

    update app.js

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+
 import Home from "./pages/Home/Home";
 import Shoes from "./components/Shoes/Shoes";
 import Men from "./pages/Men/Men";
@@ -15,109 +17,98 @@ import ReturnsExchange from "./pages/ReturnsExchange/ReturnsExchange";
 import SizeGuide from "./pages/SizeGuide/SizeGuide";
 import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 import TermsConditions from "./pages/TermsConditions/TermsConditions";
+
 import Cart from "./pages/Cart/Cart";
 import Login from "./pages/Login/Login";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import MyOrder from "./pages/MyOrder/MyOrder";
+
 import NewArrival from "./pages/NewArrival/NewArrival";
 import Offers from "./pages/Offers/Offers";
 
 function App() {
   return (
     <>
-
       <Navbar />
 
-      <Routes>
+      <main className="app-main">
+        <Routes>
 
-        <Route
-          path="/"
-          element={<Home />}
-        />
+          <Route path="/" element={<Home />} />
 
-      <Route
-          path="/shoes"
-          element={<Shoes />}
-        />
+          <Route path="/shoes" element={<Shoes />} />
 
-      <Route
-          path="/men"
-          element={<Men />}
-        />
+          <Route path="/men" element={<Men />} />
 
-      <Route
-          path="/women"
-          element={<Women />}
-        />
+          <Route path="/women" element={<Women />} />
 
-      <Route
-          path="/kids"
-          element={<Kids />}
-        />
+          <Route path="/kids" element={<Kids />} />
 
-        <Route 
-        path="/about" 
-        element={<About />} />
+          <Route path="/about" element={<About />} />
 
-        <Route 
-        path="/contact" 
-         element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
 
-        <Route 
-        path="/faq" 
-        element={<FAQ />} />
+          <Route path="/faq" element={<FAQ />} />
 
-        <Route
-        path="/shipping-delivery"
-        element={<ShippingDelivery />}
-        />
+          <Route
+            path="/shipping-delivery"
+            element={<ShippingDelivery />}
+          />
 
-        <Route
-        path="/returns-exchange"
-        element={<ReturnsExchange />}
-        />
+          <Route
+            path="/returns-exchange"
+            element={<ReturnsExchange />}
+          />
 
-        <Route
-        path="/size-guide"
-        element={<SizeGuide />}
-         />
+          <Route
+            path="/size-guide"
+            element={<SizeGuide />}
+          />
 
-        <Route
-        path="/privacy-policy"
-        element={<PrivacyPolicy />}
-        />
+          <Route
+            path="/privacy-policy"
+            element={<PrivacyPolicy />}
+          />
 
-        <Route
-        path="/terms-conditions"
-        element={<TermsConditions />}
-        />
+          <Route
+            path="/terms-conditions"
+            element={<TermsConditions />}
+          />
 
-        <Route
-        path="/cart"
-        element={<Cart />}
-        />
+          <Route
+            path="/cart"
+            element={<Cart />}
+          />
 
-        <Route
-        path="/login"
-        element={<Login />}
-        />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
 
-        <Route
-        path="/place-order"
-        element={<PlaceOrder />}
-         />
+          <Route
+            path="/place-order"
+            element={<PlaceOrder />}
+          />
 
-        <Route
-        path="/my-orders"
-        element={<MyOrder />}
-         />
+          <Route
+            path="/my-orders"
+            element={<MyOrder />}
+          />
 
-        <Route path="/newarrival" element={<NewArrival />} />
+          <Route
+            path="/newarrival"
+            element={<NewArrival />}
+          />
 
-         <Route path="/offers" element={<Offers />} />
+          <Route
+            path="/offers"
+            element={<Offers />}
+          />
 
-      </Routes>
-       <Footer />
+        </Routes>
+      </main>
+
+      <Footer />
     </>
   );
 }
