1d71e325a7baec8ea0ae4a6568f63aa7b2a96392
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-09-21 20:29:10 +0500

    add category component in home page

--- frontened/src/pages/Home/Home.jsx
+++ frontened/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Category from "../../components/Category/Category";
 import Header from "../../components/Header/Header";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      
+      <Category />
       <Header />
     </>
   );
