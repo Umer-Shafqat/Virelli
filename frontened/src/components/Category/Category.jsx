88b1ace40748621b3d5a5d62b9d809bad9763f27
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-09-21 20:43:37 +0500

    edit category

--- frontened/src/components/Category/Category.jsx
+++ frontened/src/components/Category/Category.jsx
@@ -2,33 +2,39 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Category.css";
 
+import shoesCategory from "../../assets/shoes-category.jpg";
+import jacketsCategory from "../../assets/jackets-category.jpg";
+import hoodiesCategory from "../../assets/hoodies-category.jpg";
+import watchesCategory from "../../assets/watches-category.jpg";
+import capsCategory from "../../assets/caps-category.jpg";
+
 const Category = () => {
   const navigate = useNavigate();
 
   const categories = [
     {
       name: "Shoes",
-      image: "/images/shoes-category.jpg",
+      image: shoesCategory,
       path: "/shoes",
     },
     {
       name: "Jackets",
-      image: "/images/jackets-category.jpg",
+      image: jacketsCategory,
       path: "/shoes",
     },
     {
       name: "Hoodies",
-      image: "/images/hoodies-category.jpg",
+      image: hoodiesCategory,
       path: "/shoes",
     },
     {
       name: "Watches",
-      image: "/images/watches-category.jpg",
+      image: watchesCategory,
       path: "/shoes",
     },
     {
       name: "Caps",
-      image: "/images/caps-category.jpg",
+      image: capsCategory,
       path: "/shoes",
     },
   ];
